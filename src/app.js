538e99de52afb9c0a96aeea04c9db405a5bc9b0e
Anderson Lemos <anderson.lemos@amedigital.com>
2020-09-11 19:03:19 -0300

    Configurando projeto

---
+++ src/app.js
@@ -0,0 +1,14 @@
+
+'use strict'
+'use strict'
+
+import React from 'react'
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <div>Aplicação 2</div>
+    )
+  }
+})
+export default App
