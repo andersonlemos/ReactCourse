bd05be0253b6ff2edb30a9458dfbc5826a2b418b
Anderson Lemos <anderson.lemos@amedigital.com>
2020-09-15 19:13:36 -0300

    hidlren prop in component

--- src/app.js
+++ src/app.js
@@ -5,15 +5,17 @@
 import React, { Component } from 'react'
 import Title from './title'
 import Square from './square'
+import Button from './button'
 
 class App extends Component {
   render () {
     return (
-      <div className='container'>
+      <div className='container' onClick={() => { alert('clicou') }}>
         <Title name='dunha' />
         {['red', 'green', 'blue'].map((source, index) => (
           <Square key={index} color={source} />
         ))}
+        <Button>Botão</Button>
       </div>
     )
   }
