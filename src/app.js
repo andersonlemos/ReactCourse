fd05857f9fb8f7f6c9d08d2a1cb8f33765f53770
Anderson Lemos <anderson.lemos@amedigital.com>
2020-09-15 19:44:20 -0300

    Manipulate component state with setState method

--- src/app.js
+++ src/app.js
@@ -3,21 +3,19 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Title from './title'
-import Square from './square'
-import LikeButton from './likeButton'
-import SearchButton from './searchButton'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      text: 'Bem Vindo'
+    }
+  }
+
   render () {
     return (
-      <div className='container'>
-        <Title name='dunha' />
-        {['red', 'green', 'blue'].map((source, index) => (
-          <Square key={index} color={source} />
-        ))}
-        <LikeButton />
-        <SearchButton />
+      <div className='container' onClick={() => this.setState({ text: 'Outro texto' })}>
+        {this.state.text}
       </div>
     )
   }
